8f1b1ec3ec54990f2aab2b12e545ad9f11d7a1c3
Rijan Timsina <regiontimsina@gmail.com>
2022-05-01 13:27:57 -0400

    added a separate config file

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 This is a python script I wrote to clean/organize the downloads folder.
 
 rename the clean.bat.sample file to clean.bat and set the path correctly for the script to work.
+
+set the parth to the downloads folder in config.py
--- clean.py
+++ clean.py
@@ -2,12 +2,10 @@ import os
 from os import walk
 import shutil
 import time
-
-# downloads folder path
-DOWNLOADS_PATH = r"download folder path"
+import config
 
 
-def get_file_names(path=DOWNLOADS_PATH):
+def get_file_names(path=config.DOWNLOAD_PATH):
     """This function adds all the files in a set directory to a list."""
     file_names = []
     for (dirpath, dirnames, filenames) in walk(path):
@@ -18,15 +16,15 @@ def get_file_names(path=DOWNLOADS_PATH):
 
 def find_keys(file_names):
     """This function looks at each of the files in the list the function above creates,
-            and stores the file extension on a list. This will later be used to create
-            the dir where the respective files will move to."""
+    and stores the file extension on a list. This will later be used to create
+    the dir where the respective files will move to."""
     keys = []
     for items in file_names:
         dot = items.rindex(".")
-        if items[dot+1:] in keys:
+        if items[dot + 1 :] in keys:
             pass
         else:
-            keys.append(items[dot+1:])
+            keys.append(items[dot + 1 :])
     return keys
 
 
@@ -35,7 +33,7 @@ def create_dir(dirnames, keys):
     count = 0
     for i in range(len(keys)):
         if keys[i] not in dirnames:
-            path = DOWNLOADS_PATH + r"/" + keys[i]
+            path = config.DOWNLOAD_PATH + r"/" + keys[i]
             try:
                 os.makedirs(path)
             except IOError:
@@ -52,33 +50,42 @@ def move_to_dirs(keys, values):
         for j in range(len(values)):
             if keys[i] in values[j]:
                 try:
-                    shutil.move(DOWNLOADS_PATH + r"/" + values[j],
-                                DOWNLOADS_PATH + r"/" + keys[i])
+                    shutil.move(
+                        config.DOWNLOAD_PATH + r"/" + values[j],
+                        config.DOWNLOAD_PATH + r"/" + keys[i],
+                    )
                 except Exception as e:
-                    os.remove(DOWNLOADS_PATH + r"/" + values[j])
+                    os.remove(config.DOWNLOAD_PATH + r"/" + values[j])
                     print("{} occured.".format(e))
                     print("The file was deleted.")
 
-def delete_if_older_than_a_month():
+
+def delete_if_older(num_months):
     """This function deletes the directory if the directory date modified is older than a month"""
     # get the date of the last month
-    last_month = time.strftime("%d-%m-%Y", time.localtime(time.time() - 60 * 60 * 24 * 30))
+    # last_month = time.strftime("%d-%m-%Y", time.localtime(time.time() - 60 * 60 * 24 * 30))
+    last_month = time.strftime(
+        "%d-%m-%Y", time.localtime(time.time() - 60 * 60 * 24 * num_months)
+    )
     # get the list of all the directories in the downloads folder
-    dir_list = os.listdir(DOWNLOADS_PATH)
+    dir_list = os.listdir(config.DOWNLOAD_PATH)
     # iterate through the list of directories
     for dir in dir_list:
         # get the date of the last modified file in the directory
-        last_modified_date = time.strftime("%d-%m-%Y", time.localtime(os.path.getmtime(DOWNLOADS_PATH + r"/" + dir)))
+        last_modified_date = time.strftime(
+            "%d-%m-%Y",
+            time.localtime(os.path.getmtime(config.DOWNLOAD_PATH + r"/" + dir)),
+        )
         # if the last modified date is older than a month, delete the directory
         if last_modified_date < last_month:
-            shutil.rmtree(DOWNLOADS_PATH + r"/" + dir)
+            shutil.rmtree(config.DOWNLOAD_PATH + r"/" + dir)
             print("{} was deleted.".format(dir))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     start = time.time()
     print("Getting File Names\n")
-    dirnames, file_names = get_file_names(DOWNLOADS_PATH)
+    dirnames, file_names = get_file_names(config.DOWNLOAD_PATH)
     print("{} files found in downloads folder.\n".format(len(file_names)))
     print("Getting Ready To Create Folders\n")
     keys = find_keys(file_names)
@@ -88,5 +95,5 @@ if __name__ == '__main__':
     move_to_dirs(keys, file_names)
     print("Finished\n")
     print("Deleting Folders if older than a month\n")
-    delete_if_older_than_a_month()
+    delete_if_older(config.DELETE_IF_OLDER_THAN_MONTHS)
     print("Time taken: {:.2}".format(time.time() - start))
