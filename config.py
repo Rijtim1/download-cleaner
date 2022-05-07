85b4d1593e1b5ed6986ed7d7d2a75eae5b5d5002
Rijan Timsina <regiontimsina@gmail.com>
2022-05-06 21:51:54 -0400

    removed deleting for now

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-DOWNLOAD_PATH = r"C:\Users\Downloads" # path to downloads folder
-DELETE_IF_OLDER_THAN_MONTHS = 1 # If the last modified date of the folder is older than this number of months, delete the folder
+DOWNLOAD_PATH = r"C:\Users\rijan\Downloads" # path to downloads folder
+# DELETE_IF_OLDER_THAN_MONTHS = 1 # If the last modified date of the folder is older than this number of months, delete the folder
