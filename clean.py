bf91561b91aba5ea532992828c3b23a482ff41fe
Rijan Timsina <regiontimsina@gmail.com>
2022-05-08 21:00:27 -0400

    Update clean.py

--- clean.py
+++ clean.py
@@ -5,18 +5,19 @@ import time
 
 
 class Clean:
+
     def __init__(self):
         self.file_names = []
         self.keys = []
         self.start = time.time()
-        self.debug = True
+        self.debug = False
 
     def get_file_names(self):
         """This function gets the file names in the downloads folder."""
-        self.root_dir, self.dirs, self.file_names = os.walk(DOWNLOAD_PATH).__next__()
+        self.root_dir, self.dirs, self.file_names = os.walk(
+            DOWNLOAD_PATH).__next__()
         if self.debug:
             print(self.root_dir, self.dirs, self.file_names)
-
 
     def find_file_extension(self):
         """This function finds the keys in the file names."""
@@ -43,10 +44,16 @@ class Clean:
         for i in range(len(self.file_names)):
             _, extension = self.file_names[i].split(".")
             if extension in self.keys:
-                shutil.move(os.path.join(self.root_dir, self.file_names[i]), os.path.join(self.root_dir, extension))
-                count += 1
+                try:
+                    shutil.move(
+                        os.path.join(self.root_dir, self.file_names[i]),
+                        os.path.join(self.root_dir, extension))
+                    count += 1
+                except Exception as e:
+                    print(e + "deleting file...")
+                    # delete the file
+                    os.remove(os.path.join(self.root_dir, self.file_names[i]))
         print("{} files were moved.".format(count))
-
 
     def run(self):
         """This function runs the program."""
