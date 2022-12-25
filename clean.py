9d80473abe162a6f22625e593ed0a9009e7305c5
Rijan Timsina <regiontimsina@gmail.com>
2022-12-25 10:31:38 -0500

    multiprocessing + optimization

--- clean.py
+++ clean.py
@@ -3,6 +3,7 @@ import os
 import shutil
 import time
 import user_path_util
+import multiprocessing
 
 class Clean:
     def __init__(self, path):
@@ -12,7 +13,6 @@ class Clean:
     def list_files(self):
         items = os.listdir(self.path)
         self.files = [item for item in items if os.path.isfile(f"{self.path}\\{item}")]
-        # print(self.files)
         
     def get_file_extension(self):
         self.extentions = []
@@ -29,23 +29,15 @@ class Clean:
                     extension_dir = f"{category_dir}\\{ext}"
                     if not os.path.exists(extension_dir):
                         os.mkdir(extension_dir)
-        
-    def make_dictionary(self):
-        self.file_dict = {}
-        for ext in self.extentions:
-            self.file_dict[ext] = []
-            for files in self.files:
-                if files.endswith(ext):
-                    self.file_dict[ext].append(files)            
-       
-    def move_files(self):
+    
+    def move_files(self, files):
         # Create a mapping of file extensions to file categories
         extension_map = {}
         for category, extensions in file_categories.items():
             for extension in extensions:
                 extension_map[extension] = category
         # Move the files to the correct folders
-        for file in self.files:
+        for file in files:
             # Get the file extension
             extension = os.path.splitext(file)[1]
             # Check if the file extension is in the extension map
@@ -60,21 +52,28 @@ class Clean:
                 except shutil.Error:
                     os.remove(os.path.join(self.path, file))
 
+
 def main():
     # Use saved path or prompt user for path
     path = user_path_util.get_path()
     # Check if the path is valid
     if os.path.exists(path):
         # Organize the folder at the specified path
-        # organize_folder(path)
         print(f"Organizing folder at {path}")
         clean = Clean(path)
-        # clean.setup()
         clean.list_files()
         clean.get_file_extension()
         clean.setup()
-        clean.move_files()
-        # clean.clean_up()
+        # Split the list of files into chunks
+        chunk_size = len(clean.files) // multiprocessing.cpu_count()
+        if chunk_size == 0:
+            file_chunks = [clean.files]
+        else:
+            file_chunks = [clean.files[i:i + chunk_size] for i in range(0, len(clean.files), chunk_size)]
+        # Create a pool of worker processes
+        with multiprocessing.Pool() as pool:
+            # Use the map() method to apply the move_files() method to each chunk of files in parallel
+            pool.map(clean.move_files, file_chunks)
     else:
         print(f"Error: path {path} does not exist.")
 
