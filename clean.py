b4253bd33fd0522fcbb0222202e825426ba3e129
Rijan Timsina <regiontimsina@gmail.com>
2022-12-25 10:20:04 -0500

    v3 complete

--- clean.py
+++ clean.py
@@ -2,91 +2,81 @@ from config import *
 import os
 import shutil
 import time
-import datetime
-
+import user_path_util
 
 class Clean:
-
-    def __init__(self):
-        self.file_names = []
-        self.keys = []
+    def __init__(self, path):
+        self.path = path
         self.start = time.time()
-        self.debug = False
-
-    def get_file_names(self):
-        """This function gets the file names in the downloads folder."""
-        self.root_dir = DOWNLOAD_PATH
-        with os.scandir(self.root_dir) as entries:
-            self.file_names = [entry.name for entry in entries if entry.is_file()]
-        if self.debug:
-            print(self.root_dir, self.file_names)
-
-
-    def find_file_extension(self):
-        """This function finds the file extensions and stores them in a dictionary."""
-        extension_dict = {}
-        for i in range(len(self.file_names)):
-            extension = self.file_names[i].split(".")[-1]
-            if extension not in extension_dict:
-                extension_dict[extension] = os.path.join(self.root_dir, extension)
-        self.extension_dict = extension_dict
-        if self.debug:
-            print(self.extension_dict)
-
-    def create_dir(self):
-        """This function creates the directories, if they do not already exist."""
-        count = 0
-        for i in range(len(self.keys)):
-            if self.keys[i] not in self.dirs:
+                            
+    def list_files(self):
+        items = os.listdir(self.path)
+        self.files = [item for item in items if os.path.isfile(f"{self.path}\\{item}")]
+        # print(self.files)
+        
+    def get_file_extension(self):
+        self.extentions = []
+        for files in self.files:
+            self.extentions.append(os.path.splitext(files)[1])
+        
+    def setup(self):
+        for ext in self.extentions:
+            for key, value in file_categories.items():
+                if ext in value:
+                    category_dir = f"{self.path}\\{key}"
+                    if not os.path.exists(category_dir):
+                        os.mkdir(category_dir)
+                    extension_dir = f"{category_dir}\\{ext}"
+                    if not os.path.exists(extension_dir):
+                        os.mkdir(extension_dir)
+        
+    def make_dictionary(self):
+        self.file_dict = {}
+        for ext in self.extentions:
+            self.file_dict[ext] = []
+            for files in self.files:
+                if files.endswith(ext):
+                    self.file_dict[ext].append(files)            
+       
+    def move_files(self):
+        # Create a mapping of file extensions to file categories
+        extension_map = {}
+        for category, extensions in file_categories.items():
+            for extension in extensions:
+                extension_map[extension] = category
+        # Move the files to the correct folders
+        for file in self.files:
+            # Get the file extension
+            extension = os.path.splitext(file)[1]
+            # Check if the file extension is in the extension map
+            if extension in extension_map:
+                # Get the file category for the extension
+                category = extension_map[extension]
+                # Build the destination path for the file
+                destination = os.path.join(self.path, category, extension)
+                # Move the file to the destination
                 try:
-                    os.mkdir(os.path.join(self.root_dir, self.keys[i]))
-                    count += 1
-                except FileExistsError:
-                    # the file/directory with the same name already exists
-                    pass
-        print("{} directories were created.".format(count))
+                    shutil.move(os.path.join(self.path, file), destination)
+                except shutil.Error:
+                    os.remove(os.path.join(self.path, file))
 
-
-    def move_to_dirs(self):
-        """This function moves the files to their respective directories, and removes duplicates."""
-        count = 0
-        for file_name in self.file_names:
-            extension = file_name.split(".")[-1]
-            file_path = os.path.join(self.root_dir, file_name)
-            destination_path = self.extension_dict[extension]
-            if os.path.exists(os.path.join(destination_path, file_name)):
-                # delete the duplicate file
-                os.remove(file_path)
-            else:
-                try:
-                    shutil.move(file_path, destination_path)
-                    count += 1
-                except Exception as e:
-                    print(str(e) + " deleting file...")
-                    # delete the file
-                    os.remove(file_path)
-        print("{} files were moved.".format(count))
-
-    def deletion_check(self):
-        """This function checks if the files are older than the specified number of months."""
-        last_month = datetime.datetime.now() - datetime.timedelta(days=DELETE_IF_UNUSED_AFTER)
-        count = 0
-        for i in range(len(self.dirs)):
-            if self.dirs[i] in self.keys:
-                if os.path.getmtime(os.path.join(self.root_dir, self.dirs[i])) < last_month:
-                    shutil.rmtree(os.path.join(self.root_dir, self.dirs[i]))
-                    count += 1
-        print("{} directories were deleted.".format(count))
-
-    def run(self):
-        """This function runs the program."""
-        self.get_file_names()
-        self.find_file_extension()
-        self.create_dir()
-        self.move_to_dirs()
-        # self.deletion_check()
-        print("{} seconds were taken.".format(time.time() - self.start))
-
+def main():
+    # Use saved path or prompt user for path
+    path = user_path_util.get_path()
+    # Check if the path is valid
+    if os.path.exists(path):
+        # Organize the folder at the specified path
+        # organize_folder(path)
+        print(f"Organizing folder at {path}")
+        clean = Clean(path)
+        # clean.setup()
+        clean.list_files()
+        clean.get_file_extension()
+        clean.setup()
+        clean.move_files()
+        # clean.clean_up()
+    else:
+        print(f"Error: path {path} does not exist.")
 
 if __name__ == "__main__":
-    Clean().run()
+    main()
--- config.py
+++ config.py
@@ -1,2 +1,14 @@
-DOWNLOAD_PATH = r"C:\Users\rijan\Downloads" # path to downloads folder
-DELETE_IF_UNUSED_AFTER = 30 # days
+file_categories = {
+    'Documents': ['.doc', '.docx', '.pdf', '.txt', '.rtf', '.odt'],
+    'Spreadsheets': ['.xls', '.xlsx', '.ods'],
+    'Presentations': ['.ppt', '.pptx', '.odp'],
+    'Images': ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff'],
+    'Videos': ['.mp4', '.mkv', '.avi', '.mov', '.wmv', '.flv'],
+    'Music': ['.mp3', '.wav', '.aac', '.m4a', '.flac'],
+    'Archives': ['.zip', '.rar', '.7z', '.tar', '.gz'],
+    'Executables': ['.exe', '.msi', '.dmg'],
+    'Scripts': ['.py', '.sh', '.js', '.php', '.pl'],
+    'Databases': ['.db', '.sqlite', '.mdb'],
+    'Webpages': ['.html', '.htm', '.css', '.xml'],
+    'Misc': []
+}
