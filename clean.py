272b00b2b968db065c44be52a1fd79eded217aa2
Rijan Timsina <regiontimsina@gmail.com>
2022-12-25 11:22:31 -0500

    added the contents of config file into the main script

--- clean.py
+++ clean.py
@@ -1,4 +1,3 @@
-from config import *
 import os
 import shutil
 import time
@@ -6,6 +5,21 @@ import user_path_util
 import multiprocessing
 import tqdm
 import argparse
+
+file_categories = {
+    'Documents': ['.doc', '.docx', '.pdf', '.txt', '.rtf', '.odt'],
+    'Spreadsheets': ['.xls', '.xlsx', '.ods'],
+    'Presentations': ['.ppt', '.pptx', '.odp'],
+    'Images': ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff'],
+    'Videos': ['.mp4', '.mkv', '.avi', '.mov', '.wmv', '.flv'],
+    'Music': ['.mp3', '.wav', '.aac', '.m4a', '.flac'],
+    'Archives': ['.zip', '.rar', '.7z', '.tar', '.gz'],
+    'Executables': ['.exe', '.msi', '.dmg'],
+    'Scripts': ['.py', '.sh', '.js', '.php', '.pl'],
+    'Databases': ['.db', '.sqlite', '.mdb'],
+    'Webpages': ['.html', '.htm', '.css', '.xml'],
+    'Misc': []
+}
 
 class Clean:
     def __init__(self, path):
