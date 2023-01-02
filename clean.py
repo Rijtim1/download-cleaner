192cf03918312cdfc3fd4dc2351783aca44fbf9e
Rijan Timsina <regiontimsina@gmail.com>
2023-01-02 14:51:19 -0500

    Update clean.py

--- clean.py
+++ clean.py
@@ -5,21 +5,15 @@ import user_path_util
 import tqdm
 import argparse
 import user_path_util
+import configparser
 
-file_categories = {
-    'Documents': ['.doc', '.docx', '.pdf', '.txt', '.rtf', '.odt'],
-    'Spreadsheets': ['.xls', '.xlsx', '.ods'],
-    'Presentations': ['.ppt', '.pptx', '.odp'],
-    'Images': ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff'],
-    'Videos': ['.mp4', '.mkv', '.avi', '.mov', '.wmv', '.flv'],
-    'Music': ['.mp3', '.wav', '.aac', '.m4a', '.flac'],
-    'Archives': ['.zip', '.rar', '.7z', '.tar', '.gz'],
-    'Executables': ['.exe', '.msi', '.dmg'],
-    'Scripts': ['.py', '.sh', '.js', '.php', '.pl'],
-    'Databases': ['.db', '.sqlite', '.mdb'],
-    'Webpages': ['.html', '.htm', '.css', '.xml'],
-    'Misc': []
-}
+config = configparser.ConfigParser()
+config.read('file_categories.cfg    ')
+
+file_categories = {}
+
+for caterory, file_types in config['Categories'].items():
+    file_categories[caterory] = file_types.split(',')
 
 
 class Clean:
